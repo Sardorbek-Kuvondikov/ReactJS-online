b946dda26484d90b933291893725cd189574523c
S1ardor <quvondiqovs06@gmail.com>
2024-08-16 11:39:38 +1000

    Add lesson-13 useReducer

---
+++ src/useReducer.jsx
@@ -0,0 +1,39 @@
+import { React, useState, useReducer } from "react";
+
+export default function Reducer() {
+  const [option, setOption] = useState(1);
+  let [count, dispatch] = useReducer((state, action) => {
+    return state + action.payload;
+  }, 0);
+
+  const onSelect = (evt) => {
+    setOption(Number(evt.target.value));
+  };
+
+  return (
+    <>
+      <div className="text-center">
+        <h1 className="mt-3 text-3xl">useReducer</h1>
+        <p className="mt-3 p-1 px-5 rounded-md bg-green-300 inline-block text-xl text-green-700">
+          {count}
+        </p>
+        <div className="mt-3 block">
+          <select
+            onChange={onSelect}
+            className="p-1 px-3 border-zinc-400 rounded-md border outline-none cursor-pointer"
+          >
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+          </select>
+          <button
+            onClick={() => dispatch({ payload: option })}
+            className="ml-3 p-1 px-3 border border-zinc-400 rounded-md"
+          >
+            Click {option}
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
