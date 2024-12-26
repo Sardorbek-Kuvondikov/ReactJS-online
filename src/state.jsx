03f54160d5b68963213565c2a03f9ab1fef8e8c6
S1ardor <quvondiqovs06@gmail.com>
2024-12-27 07:37:45 +1000

    Added: lesson-6-7

---
+++ src/state.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { One } from "./stateStyled";
+
+export default class State extends Component {
+  render() {
+    return (
+      <div>
+        <One>Styled Components</One>
+      </div>
+    );
+  }
+}
---
+++ src/stateStyled.js
@@ -0,0 +1,8 @@
+import styled, { css, keyframes } from "styled-components";
+
+export const One = styled.h1`
+  padding: 10px;
+  text-align: center;
+  background-color: darkcyan;
+  color: white;
+`;
